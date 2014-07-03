a6c8d076a67276c71d324bafd924c0e84e07d784
acrylian <wartgart@gmx.net>
2014-07-03 15:47:30 +0200

    Initial commit

---
+++ scriptless-socialsharing.php
@@ -0,0 +1,150 @@
+<?php
+/**
+ * A plugin that provides scriptless and privacy friendly sharing buttons for 
+ * Facebook, Twitter, Google+, Pinterest, Linkedin and Xing. 
+ * 
+ * To have it work correctly you need to enable the html_meta_tags plugin 
+ * and the Open Graph (og:) meta data elements.
+ *
+ * The plugin loads an default CSS styling using an icon font optionally (Thanks to icomoon.io). 
+ * If you wish to use theme based custom icons and css to avoid extra loading you can disable it.
+ *
+ * Usage:
+ * Place <?php printScriptlessSocialSharingButtons(); ?> on your theme files where you wish the buttons to appear.
+ *
+ * @license GPL v3
+ * @author Malte Müller (acrylian)
+ *
+ * @package plugins
+ * @subpackage social
+ */
+$plugin_is_filter = 9 | THEME_PLUGIN;
+$plugin_description = gettext('A plugin that provides scriptless and privacy friendly sharing buttons for Facebook, Twitter, Google+, Pinterest, Linkedin and Xing.');
+$plugin_author = 'Malte Müller (acrylian)';
+$plugin_version = '1.0';
+$option_interface = 'scriptless_socialsharing_options';
+if (getOption('scriptless_socialsharing_iconfont')) {
+  zp_register_filter('theme_head', 'scriptlesssocialsharingCSS');
+}
+
+class scriptless_socialsharing_options {
+
+  /**
+   * class instantiation function
+   */
+  function __construct() {
+    
+  }
+
+  function getOptionsSupported() {
+    $options = array(
+        gettext('Social networks') => array(
+            'key' => 'scriptless_socialsharing_socialnetworks',
+            'type' => OPTION_TYPE_CHECKBOX_UL,
+            'order' => 0,
+            'checkboxes' => array(// The definition of the checkboxes
+                'Facebook' => 'scriptless_socialsharing_facebook',
+                'Twitter' => 'scriptless_socialsharing_twitter',
+                'Google+' => 'scriptless_socialsharing_gplus',
+                'Pinterest' => 'scriptless_socialsharing_pinterest',
+                'linkedin' => 'scriptless_socialsharing_linkedin',
+                'xing' => 'scriptless_socialsharing_xing'
+            ),
+            'desc' => gettext('Select the social networks you wish buttons to appear for.')),
+        gettext('Icon font and default CSS')
+        => array('key' => 'scriptless_socialsharing_iconfont',
+            'type' => OPTION_TYPE_CHECKBOX,
+            'order' => 1,
+            'desc' => gettext("Uncheck to disable loading to use your own theme based icon font."))
+    );
+    return $options;
+  }
+
+}
+
+function scriptlesssocialsharingCSS() {
+  ?>
+  <link rel="stylesheet" href="<?php echo FULLWEBPATH . '/' . USER_PLUGIN_FOLDER; ?>/scriptless-socialsharing/style.css" type="text/css">
+  <!--[if lt IE 8]><!-->
+  <link rel="stylesheet" href="<?php echo FULLWEBPATH . '/' . USER_PLUGIN_FOLDER; ?>/scriptless-socialsharing/ie7.css">
+  <!--<![endif]-->
+  <?php
+}
+
+/**
+ * Place this where you wish the buttons to appear. The plugin includes also jQUery calls to set the buttons up to allow multiple button sets per page.
+ *  
+ * @param string $text Text to be displayed before the sharing list. HTML code allowed. Default "<h4>Share</h4>"
+ */
+function printScriptlessSocialSharingButtons($text = '<h4>Share</h4>') {
+  global $_zp_gallery, $_zp_gallery_page, $_zp_current_album, $_zp_current_image, $_zp_current_zenpage_news, $_zp_current_zenpage_page, $_zp_current_category;
+  $title = '';
+  $desc = '';
+  $url = '';
+  $imgsource = '';
+  switch ($_zp_gallery_page) {
+    default:
+    case 'index.php':
+      $url = getGalleryIndexURL();
+      $content = getBareGalleryTitle();
+      break;
+    case 'album.php':
+      $url = $_zp_current_album->getLink();
+      $title = $_zp_current_album->getTitle();
+      break;
+    case 'image.php':
+      $url = $_zp_current_image->getLink();
+      $title = $_zp_current_image->getTitle();
+      break;
+    case 'news.php':
+      if (function_exists("is_NewsArticle")) {
+        if (is_NewsArticle()) {
+          $url = $_zp_current_zenpage_news->getLink();
+          $content = $_zp_current_zenpage_news->getTitle();
+        } else {
+          $url = $_zp_current_category->getLink();
+          $title = $_zp_current_category->getTitle();
+        }
+      }
+    case 'pages.php':
+      if (function_exists("is_Pages")) {
+        $url = $_zp_current_zenpage_page->getLink();
+        $title = $_zp_current_zenpage_page->getTitle();
+      }
+      break;
+  }
+  $content = strip_tags($title);
+  $desc = getContentShorten($title, 100, ' (…)', false);
+  $url = PROTOCOL . "://" . $_SERVER['HTTP_HOST'].html_encode($url);
+  if($text) {
+     echo $text; 
+  }
+  ?>
+  <ul class="scriptless_socialsharing">
+  		<?php if (getOption('scriptless_socialsharing_facebook')) { ?>
+    		<li class="icon-facebook"><a href="http://www.facebook.com/sharer/sharer.php?u=<?php echo $url; ?>" title="Facebook">Facebook</a></li>
+      <?php } ?>
+
+    <?php if (getOption('scriptless_socialsharing_twitter')) { ?>
+    		<li class="icon-twitter"><a href="https://twitter.com/intent/tweet?text=<?php echo $title; ?>&amp;url=<?php echo $url; ?>" title="Twitter">Twitter</a></li>
+    <?php } ?>
+
+    <?php if (getOption('scriptless_socialsharing_pinterest')) { ?>
+    		<li class="icon-pinterest"><a href="http://pinterest.com/pin/create/button/?url=<?php echo $url; ?>&amp;description=<?php echo $title; ?>&amp;media=<?php echo $url; ?>" title="Pinterest">Pinterest</a></li>
+    <?php } ?>
+
+    <?php if (getOption('scriptless_socialsharing_gplus')) { ?>
+    		<li class="icon-google-plus"><a href="https://plus.google.com/share?url=<?php echo $url; ?>" title="Google+">Google+</a></li>
+    <?php } ?>
+
+    <?php if (getOption('scriptless_socialsharing_linkedin')) { ?>
+    		<li class="icon-linkedin"><a href="http://www.linkedin.com/shareArticle?mini=true&url=<?php echo $url; ?>&amp;title=<?php echo $title; ?>&amp;source=<?php echo $url; ?>" title="LinkedIn">LinkedIn</a></li>
+    <?php } ?>
+
+    <?php if (getOption('scriptless_socialsharing_xing')) { ?>
+    		<li class="icon-xing"><a href="https://www.xing-share.com/app/user?op=share;sc_p=xing-share;url=<?php echo $url; ?>" title="Xing">Xing</a></li>
+      <?php } ?>
+  </ul>
+  <?php
+}
+?>
---
+++ scriptless-socialsharing/ie7.js
@@ -0,0 +1,37 @@
+/* To avoid CSS expressions while still supporting IE 7 and IE 6, use this script */
+/* The script tag referring to this file must be placed before the ending body tag. */
+
+/* Use conditional comments in order to target IE 7 and older:
+	<!--[if lt IE 8]><!-->
+	<script src="ie7/ie7.js"></script>
+	<!--<![endif]-->
+*/
+
+(function() {
+	function addIcon(el, entity) {
+		var html = el.innerHTML;
+		el.innerHTML = '<span style="font-family: \'scriptless-socialbuttons\'">' + entity + '</span>' + html;
+	}
+	var icons = {
+		'icon-facebook': '&#xe600;',
+		'icon-twitter': '&#xe601;',
+		'icon-google-plus': '&#xe602;',
+		'icon-xing': '&#xe603;',
+		'icon-pinterest': '&#xe604;',
+		'icon-linkedin': '&#xe605;',
+		'0': 0
+		},
+		els = document.getElementsByTagName('*'),
+		i, c, el;
+	for (i = 0; ; i += 1) {
+		el = els[i];
+		if(!el) {
+			break;
+		}
+		c = el.className;
+		c = c.match(/icon-[^\s'"]+/);
+		if (c && icons[c[0]]) {
+			addIcon(el, icons[c[0]]);
+		}
+	}
+}());
