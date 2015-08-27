9655134c4f40c83ede245f2ae6292d5539b03f5c
acrylian <wartgart@gmx.net>
2015-08-27 19:45:28 +0200

    Version update

--- scriptless-socialsharing.php
+++ scriptless-socialsharing.php
@@ -25,7 +25,7 @@
 $plugin_is_filter = 9 | THEME_PLUGIN;
 $plugin_description = gettext('A Zenphoto plugin that provides scriptless and privacy friendly sharing buttons for Facebook, Twitter, Google+, Pinterest, Linkedin, Xing, Reddit, Stumbleupon, Tumblr, WhatsApp (iOS only) and e-mail. (Note: No share counts because of that!).');
 $plugin_author = 'Malte Müller (acrylian)';
-$plugin_version = '1.2';
+$plugin_version = '1.3';
 $option_interface = 'scriptless_socialsharing_options';
 if (getOption('scriptless_socialsharing_iconfont')) {
 	zp_register_filter('theme_head', 'scriptlesssocialsharingCSS');
